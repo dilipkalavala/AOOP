fa104d41144c13bbe15be1be64d4a4d3c7c078c5
k.Dilip <kalavaladilip390@gmail.com>
2024-08-20 11:25:39 +0530

    Add files via upload

---
+++ PRACTICALa1/src/Loginsystem/Main.java
@@ -0,0 +1,13 @@
+package Loginsystem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logger logger = Logger.getInstance();
+	     logger.log("Application started");
+	     logger.log("This is a log message");
+	     logger.log("This is a log message");
+	}
+
+}
---
+++ PRACTICALa1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module PRACTICALa1 {
+}
